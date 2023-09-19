1b950181d02e2c4be995abb7c004e3cd02a2514b
LightKitty <383929371@qq.com>
2023-09-19 11:03:28 +0800

    init

---
+++ LightKitty.Log.Test/LoggerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LightKitty.Log.Test
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void WriteTest()
+        {
+            Logger.Debug("debug log");
+            Logger.Info("info log");
+            Logger.Warn("warn log");
+            try
+            {
+                string str = null;
+                str = str.ToString();
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("error log", ex);
+            }
+            Logger.Fatal("fatal log");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
