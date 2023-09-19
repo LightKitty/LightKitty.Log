0423829942fa01c32934206d914e7b644335b1e5
LightKitty <383929371@qq.com>
2023-09-19 11:05:41 +0800

    update

---
+++ LightKitty.Log/Logger.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightKitty.Log
+{
+    /// <summary>
+    /// log. 日志
+    /// </summary>
+    public static class Logger
+    {
+        #region private fileds
+
+        /// <summary>
+        /// Write lock. 写日志锁
+        /// </summary>
+        private static readonly object writeLock = new object();
+
+        /// <summary>
+        /// Log folder path. 日志文件夹路径
+        /// </summary>
+        private static string folderPath = "log\\";
+
+        #endregion
+
+        #region fileds
+
+        /// <summary>
+        /// The lowest write log level (Default is Debug). 最低写日志级别（默认为Debug）
+        /// </summary>
+        public static LogLevel WriteLogLevel { get; set; } = LogLevel.Debug;
+
+        #endregion
+
+        #region public methods
+
+        /// <summary>
+        /// Set log folder path. 设置日志文件夹路径
+        /// </summary>
+        /// <param name="path">Log Folder Path. 日志文件夹路径</param>
+        public static void SetFolderPath(string path)
+        {
+            folderPath = (path.EndsWith("\\") || path.EndsWith("/")) ? path : path + "\\";
+            if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath); //判断并创建日志文件夹
+        }
+
+        /// <summary>
+        /// Write debug log (Not write in release). 写调试日志（Release版本不会写）
+        /// </summary>
+        /// <param name="msg">Log message. 日志信息</param>
+        /// <param name="ex">Exception. 异常</param>
+        public static void Debug(string msg, Exception ex = null)
+        {
+            if (WriteLogLevel > LogLevel.Debug) return;
+            Task.Run(() =>
+            {
+                Write(LogLevel.Debug, msg, ex?.ToString());
+            });
+        }
+
+        /// <summary>
+        /// Write info log. 写信息日志
+        /// </summary>
+        /// <param name="msg">Log message. 日志信息</param>
+        /// <param name="ex">Exception. 异常</param>
+        public static void Info(string msg, Exception ex = null)
+        {
+            if (WriteLogLevel > LogLevel.Info) return;
+            Task.Run(() =>
+            {
+                Write(LogLevel.Info, msg, ex?.ToString());
+            });
+        }
+
+        /// <summary>
+        /// Write warn log. 写警告日志
+        /// </summary>
+        /// <param name="msg">Log message. 日志信息</param>
+        /// <param name="ex">Exception. 异常</param>
+        public static void Warn(string msg, Exception ex = null)
+        {
+            if (WriteLogLevel > LogLevel.Warn) return;
+            Task.Run(() =>
+            {
+                Write(LogLevel.Warn, msg, ex?.ToString());
+            });
+        }
+
+        /// <summary>
+        /// Write error log. 写错误日志
+        /// </summary>
+        /// <param name="msg">Log message. 日志信息</param>
+        /// <param name="ex">Exception. 异常</param>
+        public static void Error(string msg, Exception ex = null)
+        {
+            if (WriteLogLevel > LogLevel.Error) return;
+            Task.Run(() =>
+            {
+                Write(LogLevel.Error, msg, ex?.ToString());
+            });
+        }
+
+        /// <summary>
+        /// Write faltal log. 写毁灭日志
+        /// </summary>
+        /// <param name="msg">Log message. 日志信息</param>
+        /// <param name="ex">Exception. 异常</param>
+        public static void Fatal(string msg, Exception ex = null)
+        {
+            if (WriteLogLevel > LogLevel.Fatal) return;
+            Task.Run(() =>
+            {
+                Write(LogLevel.Fatal, msg, ex?.ToString());
+            });
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Write log. 写日志
+        /// </summary>
+        /// <param name="logLevel"></param>
+        /// <param name="msg"></param>
+        /// <param name="exMsg"></param>
+        private static void Write(LogLevel logLevel, string msg, string exMsg)
+        {
+            Write(GetName(), GetContent(logLevel, msg, exMsg)); //格式化日志、写日志
+        }
+
+        /// <summary>
+        /// Write log. 写日志
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="msg"></param>
+        private static void Write(string name, string msg)
+        {
+            string path = folderPath + name; //日志路径
+            lock (writeLock)
+            { //保证单线程写磁盘文件
+                if (File.Exists(path))
+                { //存在日志文件
+                    using (StreamWriter sw = new StreamWriter(path, true, Encoding.UTF8)) //追加
+                    {
+                        sw.Write(msg); //写日志
+                    }
+                }
+                else
+                { //不存在日志文件
+                    if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath); //判断并创建日志文件夹
+                    using (var fs = File.Create(path)) //创建日志文件
+                    using (StreamWriter sw = new StreamWriter(fs, Encoding.UTF8))
+                    {
+                        sw.Write(msg); //写日志
+                    } 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get log file name. 获取日志文件名称
+        /// </summary>
+        /// <returns></returns>
+        private static string GetName()
+        {
+            return DateTime.Now.ToString("yyyyMMdd") + ".log";
+        }
+
+        /// <summary>
+        /// Format log content. 格式化日志内容
+        /// </summary>
+        /// <param name="logLevel"></param>
+        /// <param name="msg"></param>
+        /// <param name="exMsg"></param>
+        /// <returns></returns>
+        private static string GetContent(LogLevel logLevel, string msg, string exMsg)
+        {
+            if (string.IsNullOrEmpty(exMsg))
+            {
+                return GetHead(logLevel) + msg + Environment.NewLine;
+            }
+            else
+            {
+                return GetHead(logLevel) + msg + Environment.NewLine + exMsg + Environment.NewLine;
+            }
+        }
+
+        /// <summary>
+        /// Log head text. 日志头部文本
+        /// </summary>
+        /// <param name="logLevel"></param>
+        /// <returns></returns>
+        private static string GetHead(LogLevel logLevel)
+        {
+            return $"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")} {logLevel.ToString()}] ";
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Log level. 日志级别
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Debug level. 调试级别
+        /// </summary>
+        Debug,
+
+        /// <summary>
+        /// Info level. 信息级别
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// Warn level. 警告级别
+        /// </summary>
+        Warn,
+
+        /// <summary>
+        /// Error level, 错误级别
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Fatal level. 毁灭级别
+        /// </summary>
+        Fatal
+    };
+}
